e5d3ea5f794a3dd4337e82e8f0048ce1746047f4
fayzgo63-link <fayzgo63@gmail.com>
2026-07-14 15:09:50 +0700

    Update sweety.py

--- sweety.py
+++ sweety.py
@@ -92,7 +92,7 @@ class httpth1(threading.Thread):
                 randomized_url = url + "?" + genstr(random.randint(3, 10))
                 requests.get(randomized_url, headers=headers)
                 u += 1
-                print("\033[48;5;1m\033[37m" +str(u)+ "\033[0m \033[37mrequests: \033[38;5;6mm" +(url)+ "\033[0m") 
+                print("\033[48;5;1m\033[37m" +str(u)+ "\033[0m \033[37mrequests: \033[38;5;6mGET randomized_url\033[0m") 
             except requests.exceptions.ConnectionError:
                 print("\033[48;5;3m\033[30" +(url)+ "\033[0m \033[31mrequests error!\033[0m")
                 pass
