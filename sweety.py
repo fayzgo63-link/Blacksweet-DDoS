f6a76c4ea5b664f07cf5b642e58d44484addc79e
fayzgo63-link <fayzgo63@gmail.com>
2026-07-14 16:28:31 +0700

    Fix print statement to use parentheses for variable

--- sweety.py
+++ sweety.py
@@ -92,7 +92,7 @@ class httpth1(threading.Thread):
                 randomized_url = url + "?" + genstr(random.randint(3, 10))
                 requests.get(randomized_url, headers=headers)
                 u += 1
-                print(f"\033[38;5;220m[B'sweet]\033[37m->Target: \033[36m" +url+ " \033[32mRequest: \033[96m +u+ \033[0m")
+                print(f"\033[38;5;220m[B'sweet]\033[37m->Target: \033[36m" +url+ " \033[32mRequest: \033[96m +(u)+ \033[0m")
             except requests.exceptions.ConnectionError:
                 print("\033[48;5;3m\033[30" +(url)+ "\033[0m \033[31mrequests error!\033[0m")
                 pass
